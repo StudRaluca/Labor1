c91d88347a8ec1d40f04b01c8ebd0ad0b3a25e2d
StudRaluca <barzaune.raluca@gmail.com>
2022-10-10 14:37:26 +0300

    Initial commit

---
+++ src/car/Car.java
@@ -0,0 +1,29 @@
+package car;
+
+public class Car {
+    private String CarBrand;
+    private String CarModel;
+    private String CarColour;
+
+    public String getBrand() {
+        return CarBrand;
+    }
+
+    public void setBrand(String brand) {
+        this.CarBrand = brand;
+    }
+
+    public String getModel() {
+        return CarModel;
+    }
+
+    public void setModel(String model) {
+        this.CarModel = model;
+    }
+
+    public String getColour() {
+        return CarColour;
+    }
+
+
+}
